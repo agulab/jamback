e4da7cccea9bba438a763a8405144b0f1ff4538f
Juan Mendoza <jmendoza@despegar.com>
2016-11-08 20:34:09 -0300

    Se modifica package de configuracion

--- src/main/java/com/jamback/config/Launcher.java
+++ src/main/java/com/jamback/config/Launcher.java
@@ -1,4 +1,4 @@
-package com.jamback.jpa;
+package com.jamback.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
